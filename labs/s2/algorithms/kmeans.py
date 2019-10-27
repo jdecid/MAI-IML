77499459f2f894932bb721e18f816cd0fe9db971
Josep de Cid <josep.de.cid@gmail.com>
2019-10-27 16:20:12 +0100

    Add KModes and some modifications on KMeans

--- labs/s2/algorithms/kmeans.py
+++ labs/s2/algorithms/kmeans.py
@@ -1,46 +1,57 @@
+import colorsys
+import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 from sklearn.preprocessing import MinMaxScaler
 
 
 class KMeans:
-    def __init__(self, K, seed=1):
+    def __init__(self, K, seed=1, visualize=False):
+        print('KMeans')
         self.K = K
         self.seed = seed
+        self.visualize = visualize
 
+        self.X = None
         self.centroids = None
+
+        colors = [(x * 1.0 / K, x * 1.0 / K, 0.5) for x in range(K)]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), colors))
+        self.colors = [plt.cm.hsv(x / K) for x in range(K)]
 
     def fit(self, X: np.ndarray, max_it=20):
         np.random.seed(self.seed)
-        self.centroids = np.random.random(size=(self.K, X.shape[1]))
+        self.X = X
+        self._init_centroids()
 
         previous_nearest_idx = None
 
         it = 0
         while it < max_it:
             print(f'Iteration {it}')
-            self.__display_iteration(X)
 
             # Calculate distance from each point to each cluster
             distances = np.zeros(shape=(self.K, X.shape[0]))
             for c_idx, centroid in enumerate(self.centroids):
                 for p_idx, point in enumerate(X):
-                    distances[c_idx, p_idx] = KMeans.__distance(centroid, point)
+                    distances[c_idx, p_idx] = self._distance(centroid, point)
 
             # Get nearest points for each cluster
             nearest = [[] for _ in range(self.K)]
-            nearest_idx = [set() for _ in range(self.K)]
+            nearest_idx = [[] for _ in range(self.K)]
 
             for p_idx, point in enumerate(X):
                 min_index = int(np.argmin(distances[:, p_idx]))
                 nearest[min_index].append(point)
-                nearest_idx[min_index].add(p_idx)
+                nearest_idx[min_index].append(p_idx)
+
+            if self.visualize:
+                self._display_iteration(X, nearest_idx)
 
             # Recalculate centroids as the mean of their nearest points
-            # TODO: Ugly AF
-            self.centroids = np.array(list(map(lambda x: np.mean(np.array(x), axis=0), nearest)))
+            self._compute_centroids(nearest)
 
             # Check convergence
             it += 1
@@ -49,43 +60,59 @@ class KMeans:
             else:
                 previous_nearest_idx = nearest_idx
 
-    def transform(self, X):
+            time.sleep(1)
+
+    def _compute_centroids(self, nearest):
+        # TODO: Ugly AF
+        self.centroids = np.array(list(map(lambda x: np.mean(np.array(x), axis=0), nearest)))
+
+    def predict(self, X):
         # Calculate distance from each point to each cluster
         distances = np.zeros(shape=(self.K, X.shape[0]))
         for c_idx, centroid in enumerate(self.centroids):
             for p_idx, point in enumerate(X):
-                distances[c_idx, p_idx] = KMeans.__distance(centroid, point)
+                distances[c_idx, p_idx] = self._distance(centroid, point)
 
         # Get nearest points for each cluster
         nearest = [[] for _ in range(self.K)]
 
+        classes = []
         for p_idx, point in enumerate(X):
-            min_index = int(np.argmin(distances[:, p_idx]))
-            nearest[min_index].append(point)
+            cluster_idx = int(np.argmin(distances[:, p_idx]))
+            classes.append(cluster_idx)
 
-        return nearest
+        return classes
 
-    def fit_transform(self, X):
+    def fit_predict(self, X):
         self.fit(X)
-        return self.transform(X)
+        return self.predict(X)
 
-    def __display_iteration(self, X):
+    def _init_centroids(self):
+        self.centroids = np.random.random(size=(self.K, self.X.shape[1]))
+
+    def _display_iteration(self, X, nearest_idx):
         plt.figure(figsize=(4, 4))
-        plt.scatter(X[:, 0], X[:, 1], c='blue')
-        plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red')
+        for idx in range(self.K):
+            plt.scatter(X[nearest_idx[idx], 0], X[nearest_idx[idx], 1],
+                        c=[self.colors[idx]], s=10, alpha=0.5)
+            plt.scatter(self.centroids[idx, 0], self.centroids[idx, 1],
+                        c=[self.colors[idx]], s=150, marker='*', edgecolors='black')
         plt.show()
 
     @staticmethod
-    def __distance(a, b):
+    def _distance(a, b):
         return np.linalg.norm(a - b)
 
 
-dataset = pd.read_csv('Mall_Customers.csv')
-dataset.describe()
-X = dataset.iloc[:, [3, 4]].values
-
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
-
-kmeans = KMeans(5)
-kmeans.fit_transform(X)
+"""
+if __name__ == '__main__':
+    dataset = pd.read_csv('Mall_Customers.csv')
+    dataset.describe()
+    X = dataset.iloc[:, [3, 4]].values
+    
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+    
+    kmeans = KMeans(3)
+    kmeans.fit_transform(X)
+"""
---
+++ labs/s2/algorithms/kmodes.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+from scipy import stats
+from algorithms.kmeans import KMeans
+
+
+class KModes(KMeans):
+    def _init_centroids(self):
+        idx = np.random.choice(range(self.K), size=self.K, replace=False)
+        self.centroids = self.X[idx]
+
+    def _compute_centroids(self, nearest):
+        # self.centroids = np.array(list(map(lambda x: np.mean(np.array(x), axis=0), nearest)))
+
+        for k in range(self.K):
+            if nearest[k]:
+                self.centroids[k, :] = stats.mode(nearest[k]).mode[0]
+
+    @staticmethod
+    def _distance(a, b):
+        # delta kronecker (0 if ==, else 1)
+        return np.sum(a != b)
+
+
+dataset = pd.read_csv('../datasets/connect-4-clean.csv')
+dataset.describe()
+X = dataset.iloc[:30000, :].values
+
+kmodes = KModes(3)
+kmodes.fit(X)
+print(kmodes.predict(X[:20, :]))
