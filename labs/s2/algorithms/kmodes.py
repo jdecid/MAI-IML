2144058d65d5c66f6ae4b25796e43c7534612c8c
Jordi Armengol Estapé <me@jordiae.com>
2019-10-27 20:23:52 +0100

    working on kmodes, added start of agglomerative

--- labs/s2/algorithms/kmodes.py
+++ labs/s2/algorithms/kmodes.py
@@ -3,11 +3,20 @@ import pandas as pd
 
 from scipy import stats
 from algorithms.kmeans import KMeans
-
+import collections
 
 class KModes(KMeans):
+    def __init__(self, K: int, seed=1):
+        """
+
+        :param K: Number of Clusters
+        :param seed: Fixed seed to allow reproducibility.
+        """
+        super().__init__(K, seed=seed)
+
     def _init_centroids(self):
-        idx = np.random.choice(range(self.K), size=self.K, replace=False)
+        idx = np.random.choice(range(0, self.X.shape[0]), size=self.K, replace=False)
+        print(idx)
         self.centroids = self.X[idx]
 
     def _compute_centroids(self, nearest):
@@ -20,10 +29,17 @@ class KModes(KMeans):
         return np.sum(a != b)
 
 
-dataset = pd.read_csv('../datasets/connect-4-clean.csv')
-dataset.describe()
-X = dataset.iloc[:30000, :].values
+if __name__ == '__main__':
+    dataset = pd.read_csv('../datasets/car.data')
+    X = dataset.iloc[:,:6]
 
-kmodes = KModes(3)
-kmodes.fit(X)
-print(kmodes.predict(X[:20, :]))
+    kmodes = KModes(K=4,seed=1)
+    res = kmodes.fit_predict(X.values)
+    with open('res.txt', 'w') as f:
+        f.write(str(res))
+    c = collections.Counter()
+    c.update(res)
+    print(c)
+    Y = dataset.iloc[:,6]
+    print(Y.value_counts())
+
