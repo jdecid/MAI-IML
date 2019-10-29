fdf4e4668952994be3e8383472e3da821ee02b11
Josep de Cid <josep.de.cid@gmail.com>
2019-10-29 14:35:16 +0100

    Add for vs vectorized

--- labs/s2/algorithms/fcm.py
+++ labs/s2/algorithms/fcm.py
@@ -24,44 +24,49 @@ class FuzzyCMeans(KMeans):
         u = np.random.random(size=(self.K, self.X.shape[0]))
         self.u = u / u.sum(axis=0)[None, :]
 
-    def _update_u(self, X: np.ndarray):
-        # TODO: Remove
-        # self.centroids = np.random.random(size=(self.K, X.shape[1]))
+    def _compute_centroids(self, *args):
+        """
+        Update c-partition matrix U and centroids (centers of gravity) V.
+        :param args: Ignored for inheritance interface design.
+        """
+        self._update_u()
+        self._update_v()
 
-        print(self.u, self.u.shape, '\n')
+    def _update_v(self):
+        """
+        Update centroids (centers of gravity) V.
+        v_k = ∑_i ((U_ki ^ m) * x_i) / ∑_i (U_ki ^ m)
+        """
+        centrooids = self.centroids.copy()
 
+        # u_pow_m = self.u ** self.m
+        # n_term = u_pow_m @ self.X
+        # d_term = u_pow_m.sum(axis=1, keepdims=True)
+        # self.centroids = n_term / d_term
+
+        for i in range(self.K):
+            u_pow_m = self.u[i, :] ** self.m
+            self.centroids[i, :] = (u_pow_m.dot(self.X)).sum() / u_pow_m.sum()
+
+        # print(self.centroids[:, 0])
+        # print(centrooids[:, 0])
+
+        # input()
+
+    def _update_u(self):
         # TODO: Vectorize
         for i in range(self.u.shape[0]):
             for k in range(self.u.shape[1]):
                 uik = 0
-                num = np.linalg.norm(X[k, :] - self.centroids[i])
+                num = np.linalg.norm(self.X[k, :] - self.centroids[i])
                 for j in range(self.u.shape[0]):
-                    den = np.linalg.norm(X[k, :] - self.centroids[j])
+                    den = np.linalg.norm(self.X[k, :] - self.centroids[j])
                     uik += (num / den) ** (2 / (self.m - 1))
                 uik ** -1
                 self.u[i, k] = uik
 
         # TODO: Necessary?
         self.u = self.u / self.u.sum(axis=0)[None, :]
-
-        print(self.u, self.u.shape, '\n')
-
-    def _compute_centroids(self, X):
-        # TODO: Remove
-        # self.centroids = np.random.random(size=(self.K, X.shape[1]))
-
-        # for i in range(self.K):
-        #     u_pow_m = self.u[i, :] ** self.m
-        #
-        #     self.centroids[i] = (u_pow_m.dot(X)).sum() / u_pow_m.sum()
-
-        # TODO: Test and review
-        u_pow_m = self.u ** self.m
-        num = u_pow_m @ X
-        self.centroids = num / num.sum(axis=0)
-
-        # print(self.centroids, self.centroids.shape, '\n')
-        # print(self.centroids2, self.centroids2.shape, '\n')
 
     def _display_iteration(self, X, nearest_idx):
         if self.vis_dims == 0:
@@ -89,7 +94,7 @@ if __name__ == '__main__':
     dataset = sc.fit_transform(dataset)
 
     fcm = FuzzyCMeans(C=4, m=2, vis_dims=2)
-    # kmeans.fit_predict(dataset)
-    fcm.X = dataset
-    fcm._init_u()
-    fcm._display_iteration(dataset, None)
+    fcm.fit_predict(dataset)
+    # fcm.X = dataset
+    # fcm._init_u()
+    # fcm._display_iteration(dataset, None)
--- labs/s2/knn.py
+++
@@ -1,6 +0,0 @@
-class KNN:
-    def __init__():
-        pass
-
-    def distance():
-        pass
