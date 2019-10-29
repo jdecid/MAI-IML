02c16b761e04b3a1bb7193efae29197ff499258a
Jordi Armengol Estapé <me@jordiae.com>
2019-10-29 14:27:33 +0100

    Evaluation metrics utility

--- labs/s2/evaluate.py
+++ labs/s2/evaluate.py
@@ -1,33 +1,25 @@
-from sklearn.metrics import adjusted_rand_score
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.spatial import distance
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
-
-from algorithms.kmodes import KModes
-#from algorithms.kmeans import KMeans
-def evaluate(labels_true, labels_pred):
-    return adjusted_rand_score(labels_true, labels_pred)
-
-def main():
-    dataset = pd.read_csv('datasets/car.data')
-    X = dataset.iloc[:, :6]
-    kmodes = KModes(K=4, seed=1)
-    res = kmodes.fit_predict(X.values)
-    with open('res.txt', 'w') as f:
-        f.write(str(res))
-    # c = collections.Counter()
-    # c.update(res)
-    # print(c)
-    Y = dataset.iloc[:, 6]
-    # print(Y.value_counts())
-    print(evaluate(labels_true=Y, labels_pred=res))
-    print('hola')
+from sklearn.metrics import *
 
 
-if __name__ == '__main__':
-    main()
+def evaluate_supervised(labels_true, labels_pred):
+    res = dict(adjusted_mutual_info_score=adjusted_mutual_info_score(labels_true, labels_pred),
+               adjusted_rand_score=adjusted_rand_score(labels_true, labels_pred),
+               completeness_score=completeness_score(labels_true, labels_pred),
+               contingency_matrix=cluster.contingency_matrix(labels_true, labels_pred),
+               fowlkes_mallows_score=fowlkes_mallows_score(labels_true, labels_pred),
+               homogeneity_score=homogeneity_score(labels_true, labels_pred),
+               v_measure_score=v_measure_score(labels_true, labels_pred))
+    return res
+
+
+def evaluate_unsupervised(X, labels):
+    res = dict(calinski_harabasz_score=calinski_harabasz_score(X, labels),
+               davies_bouldin_score=davies_bouldin_score(X, labels),
+               silhouette_score=silhouette_score(X, labels))
+    return res
+
+
+def evaluate(labels_true, labels_pred, X):
+    eval_sup = evaluate_supervised(labels_true, labels_pred)
+    eval_unsup = evaluate_unsupervised(X, labels_pred)
+    return eval_sup, eval_unsup
