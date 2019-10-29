2f694925e2f0fa51217d14c6776f458580bb060f
Jordi Armengol Estapé <me@jordiae.com>
2019-10-29 15:06:16 +0100

    Driver

---
+++ labs/s2/run.py
@@ -0,0 +1,11 @@
+
+def main():
+    """
+    Runs EVERYTHING (preprocessing, clustering, evaluation,...), saves images, logs, results etc for the report
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
