44218a6fde1b14b9b8693c42b3671ad30963a0b0
Jordi Armengol Estapé <me@jordiae.com>
2019-10-30 18:09:41 +0100

    Added y for supervised evaluation

--- labs/s2/preprocessing/connect_4.py
+++ labs/s2/preprocessing/connect_4.py
@@ -13,9 +13,11 @@ def preprocess():
     data = read_dataset(DATASET)
     df = pd.DataFrame(data['data'])
 
-    # since we are still doing unsupervised methods (clustering), we will ignore labels y
+    # since we are still doing unsupervised methods (clustering), we will ignore labels y... except for sup. evaluation
+    df = df.applymap(lambda x: x.decode('utf-8'))  # encode values as unicode strings instead of bytes
     X = df.loc[:, df.columns != 'class']
-    X = X.applymap(lambda x: x.decode('utf-8'))  # encode values as unicode strings instead of bytes
+    y = df.loc['class']  # for supervised evaluation of clustering
+
     # For all vars in X, the domain is ['b', 'o', 'x']
     # However, we will check it programatically.
     # Also, even if the dataset is supposed to have no missing values, we will check it as well, just in case.
@@ -46,9 +48,10 @@ def preprocess():
     X_encoded = X.applymap(recode)
 
 
-    # save the cleaned/encoded X as a CSV for later
+    # save the cleaned/encoded X as a CSV for later. y is needed for supervised evaluation.
     X.to_csv(os.path.join('datasets', 'connect-4-clean.csv'), index=False)
     X_encoded.to_csv(os.path.join('datasets', 'connect-4-clean-enc.csv'), index=False)
+    y.to_csv(os.path.join('datasets', 'connect-4-clean-y.csv'), index=False)
     return 'connect-4-clean.csv', 'connect-4-clean-enc.csv'
 
 
