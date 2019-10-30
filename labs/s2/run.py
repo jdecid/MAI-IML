afc14353081a091275bf0ef85de59a0780ec9562
Josep de Cid <josep.de.cid@gmail.com>
2019-10-30 17:27:42 +0100

    Add optimization in run

--- labs/s2/run.py
+++ labs/s2/run.py
@@ -1,10 +1,12 @@
 import os
 import logging
 import pandas as pd
+from sklearn.metrics import calinski_harabasz_score
 
 from algorithms.kmeans import KMeans
 from algorithms.kmodes import KModes
 from preprocessing import connect_4, adult, segment
+from utils.optimize import optimize
 
 
 def main():
@@ -17,18 +19,34 @@ def main():
 
     print('Preprocessing...')
     # file_c4, file_c4_enc = connect_4.preprocess()
-    # file_adult, file_adult_enc = adult.preprocess()
-    file_segment = segment.preprocess()
+    file_adult, file_adult_enc = adult.preprocess()
+    # file_segment = segment.preprocess()
 
     print('Applying hierarchical clustering...')
     # TODO
 
+    # ------------------------------------------------------ #
+
     print('KMeans')
 
-    X = pd.read_csv(os.path.join('datasets', file_segment))
+    X = pd.read_csv(os.path.join('datasets', file_adult_enc))
 
-    kmeans = KMeans(K=7, vis_dims=2)
-    kmeans.fit_predict(X.values)
+    # kmeans = KMeans(K=2, vis_dims=2)
+    # kmeans.fit_predict(X.values)
+
+    # Optimization of K
+
+    result = optimize(X=X.values,
+                      algorithm=KMeans,
+                      algorithm_params={},
+                      metric=calinski_harabasz_score,
+                      metric_params={'X': X.values},
+                      k_values=[2, 3, 4, 5],
+                      goal='minimize')
+
+    print(result)
+
+    # ------------------------------------------------------ #
 
 
 if __name__ == '__main__':
--- labs/s2/utils/optimize.py
+++ labs/s2/utils/optimize.py
@@ -1,5 +1,4 @@
-xfrom math import inf
-from algorithms.kmeans import KMeans
+from math import inf
 
 
 def init_best(goal):
@@ -12,21 +11,29 @@ def init_best(goal):
 
 def better(goal, current_best_val, new_val):
     if goal == 'maximize':
-        if new_val > current_best_val:
-            return True
+        return new_val > current_best_val
     else:
-        if new_val < current_best_val:
-            return True
-    return False
+        return new_val < current_best_val
 
 
-def optimize(X, clustering_alg, clustering_alg_params, metric, metric_params, k_values, goal):
+def optimize(X, algorithm, algorithm_params, metric, metric_params, k_values, goal):
+    """
+
+    :param X:
+    :param algorithm:
+    :param algorithm_params:
+    :param metric:
+    :param metric_params:
+    :param k_values:
+    :param goal:
+    :return:
+    """
     assert goal in ['maximize', 'minimize']
     best_val = init_best(goal)
     best_k = None
     all_vals = {}
     for k in k_values:
-        clust_alg = clustering_alg(K=k, **clustering_alg_params)
+        clust_alg = algorithm(K=k, **algorithm_params)
         clustering = clust_alg.fit_predict(X)
         metric_params['labels'] = clustering
         new_val = metric(**metric_params)
@@ -35,21 +42,3 @@ def optimize(X, clustering_alg, clustering_alg_params, metric, metric_params, k_
             best_k = k
             best_val = new_val
     return dict(best={best_k: best_val}, all_vals=all_vals)
-
-
-def main():
-    import pandas as pd
-    from sklearn.metrics import calinski_harabasz_score
-    #dataset = pd.read_csv('tests/datasets/iris.csv')
-    #y = dataset.iloc[:, 4].values
-    #X = dataset.iloc[:,:4].values
-    dataset = pd.read_csv('tests/datasets/Mall_Customers.csv')
-    X = dataset.iloc[:,[2,3,4]].values
-    from sklearn.preprocessing import MinMaxScaler
-    sc = MinMaxScaler()
-    X = sc.fit_transform(X)
-    print(optimize(X=X,clustering_alg=KMeans,clustering_alg_params={}, metric=calinski_harabasz_score, metric_params={'X':X}, k_values=[2,3,4], goal='minimize'))
-
-
-if __name__ == '__main__':
-    main()
