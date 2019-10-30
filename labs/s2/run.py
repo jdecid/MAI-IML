fbb8399092a77b48e6925fa59f1a01c0a5a10c53
Jordi Armengol Estapé <me@jordiae.com>
2019-10-30 13:12:40 +0100

    Driver

--- labs/s2/run.py
+++ labs/s2/run.py
@@ -1,10 +1,15 @@
+from preprocessing import preprocess_connect_4
+
 
 def main():
     """
     Runs EVERYTHING (preprocessing, clustering, evaluation,...), saves images, logs, results etc for the report
     :return:
     """
-    pass
+    print('Preprocessing...')
+    filename_clean, filename_clean_enc = preprocess_connect_4.preprocess()
+    print('Applying hierarchical clustering...')
+
 
 
 if __name__ == '__main__':
