85c150d6db8ac128ba8e6994cac69748cb67ad5a
Josep de Cid <josep.de.cid@gmail.com>
2019-12-21 12:07:40 +0100

    Add preprocessing

---
+++ labs/w3/preprocessing/adult.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+CATEGORICAL_FEATURES = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+
+NUMERICAL_FEATURES = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+
+def preprocess(train_dataset, validation_dataset):
+    train_data = train_dataset['data']
+    val_data = validation_dataset['data']
+
+    # TODO: Remove sample
+    df_train = pd.DataFrame(train_data)
+    df_train = df_train.sample(n=5000, replace=False, random_state=1).reset_index(drop=True)
+
+    df_val = pd.DataFrame(val_data)
+
+    df_train = df_train.applymap(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+    df_val = df_val.applymap(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+
+    # Real Y labels
+    y_train = df_train['class'].copy()
+    df_train = df_train.drop(columns=['class'])
+
+    y_val = df_val['class'].copy()
+    df_val = df_val.drop(columns=['class'])
+
+    # Encode categorical values into numerical with OHE
+    ohe = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    X_categorical_train = ohe.fit_transform(df_train[CATEGORICAL_FEATURES])
+    X_categorical_val = ohe.transform(df_val[CATEGORICAL_FEATURES])
+
+    columns = ohe.get_feature_names(input_features=CATEGORICAL_FEATURES)
+    X_categorical_train = pd.DataFrame(data=X_categorical_train, columns=columns)
+    X_categorical_val = pd.DataFrame(data=X_categorical_val, columns=columns)
+
+    # Scale numerical values
+    sc = MinMaxScaler()
+    X_numerical_train = sc.fit_transform(df_train[NUMERICAL_FEATURES])
+    X_numerical_val = sc.transform(df_val[NUMERICAL_FEATURES])
+
+    X_numerical_train = pd.DataFrame(data=X_numerical_train, columns=NUMERICAL_FEATURES)
+    X_numerical_val = pd.DataFrame(data=X_numerical_val, columns=NUMERICAL_FEATURES)
+
+    # Numerical only data
+    X_df_train = pd.concat((X_categorical_train, X_numerical_train), axis=1)
+    X_df_val = pd.concat((X_categorical_val, X_numerical_val), axis=1)
+
+    return (X_df_train, y_train), (X_df_val, y_val)
--- labs/w3/run.py
+++ labs/w3/run.py
@@ -1,20 +1,26 @@
+import os
 import argparse
 from typing import Tuple, List
 
 import numpy as np
 
-from algorithms.KIBLAlgorithm import KIBLAlgorithm
-from algorithms.reduction_KIBL_algorithm import reduction_KIBL_algorithm, REDUCTION_METHODS
+from preprocessing.adult import preprocess
 from utils.dataset import read_dataset
 
 
 def read_data(name: str) -> List[Tuple[np.ndarray, np.ndarray]]:
     folds = []
     for i in range(10):
-        train_data = read_dataset(name=f'{name}.fold.00000{i}.train')
-        validation_data = read_dataset(name=f'{name}.fold.00000{i}.test')
+        train_data = read_dataset(name=f'{name}.fold.00000{i}.train', dataset_path=os.path.join('datasets', name))
+        validation_data = read_dataset(name=f'{name}.fold.00000{i}.test', dataset_path=os.path.join('datasets', name))
 
-        folds.append((train_data, validation_data))
+        (X_train, y_train), (X_val, y_val) = preprocess(train_data, validation_data)
+        folds.append({
+            'X_train': X_train,
+            'y_train': y_train,
+            'X_val': X_val,
+            'y_val': y_val
+        })
 
     return folds
 
@@ -31,8 +37,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    X = np.zeros((0, 10))
-
-    for reduction_method in REDUCTION_METHODS:
-        U = reduction_KIBL_algorithm(X, reduction_method)
-        knn = KIBLAlgorithm(U)
+    read_data(args.dataset)
