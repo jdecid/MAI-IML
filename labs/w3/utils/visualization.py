ddcd0553cff2b4c9c56323eaaf95e4e1f7518085
acanya <arnau.canyadell@est.fib.upc.edu>
2019-12-22 14:42:48 +0100

    visualization of results k-IBL

--- labs/w3/utils/visualization.py
+++ labs/w3/utils/visualization.py
@@ -8,6 +8,7 @@ import numpy as np
 
 def visualize(dataset: str, var: str):
     assert var in ['k', 'r', 'vp', 'rp'], 'var parameters must be k, r, vp or rp'
+    assert dataset in ['hypothyroid', 'pen-based'], 'dataset must be "hypothyroid" or "pen-based"'
     var_name = 'k'
     if var == 'r':
         var_name = 'r'
@@ -17,6 +18,9 @@ def visualize(dataset: str, var: str):
         var_name = 'Retention policy'
     with open(os.path.join('../output', f'{dataset}_results.json'), mode='r') as f:
         data = json.loads(f.read())
+    dataset_name = 'Hypothyroid'
+    if dataset == 'pen-based':
+        dataset_name = 'Pen-Based'
 
     _, ax = plt.subplots(2, 1, sharex='col')
 
@@ -25,7 +29,7 @@ def visualize(dataset: str, var: str):
         mean_time = np.mean(list(map(lambda x: x['time'], result['results'])))
 
         ax[0].scatter(result[var], mean_accuracy)
-        ax[0].set_title('Performance vs ' + var_name)
+        ax[0].set_title('Performance vs ' + var_name + ' in ' + dataset_name + ' dataset')
         ax[0].set_ylabel('Accuracy')
 
         ax[1].scatter(result[var], mean_time)
@@ -37,4 +41,11 @@ def visualize(dataset: str, var: str):
 
 
 visualize('pen-based', 'k')
+visualize('pen-based', 'r')
 visualize('pen-based', 'vp')
+visualize('pen-based', 'rp')
+
+visualize('hypothyroid', 'k')
+visualize('hypothyroid', 'r')
+visualize('hypothyroid', 'vp')
+visualize('hypothyroid', 'rp')
